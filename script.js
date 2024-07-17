29ba2a4c08e5cd6abb8d070e9ded8194b1316380
Cleymer Avila <avilacleymer05@gmail.com>
2024-07-17 00:41:56 -0500

    Encriptacion, desencriptacion y estilo propio

--- script.js
+++ script.js
@@ -3,43 +3,104 @@ let textoEncriptado;
 let textoDesencriptado;
 let estadoTexto;
 let textoIngresado;
-let tarjetaResultado = document.querySelector(".card_output");
-let contenedorMensaje = document.querySelector(".container_mensaje");
-let imagen = document.getElementById("imagen");
-let titulo = document.getElementById("titulo_mensaje");
-let resultado = document.getElementById("texto_mensaje");
-let btnCopiar = document.getElementById("boton_copiar");
-
-
-
+const tarjetaResultado = document.querySelector(".card_output");
+const contenedorMensaje = document.querySelector(".container_mensaje");
+const imagen = document.getElementById("imagen");
+const titulo = document.getElementById("titulo_mensaje");
+const resultado = document.getElementById("texto_mensaje");
+const btnCopiar = document.getElementById("boton_copiar");
+const clavesEncriptado = {
+    'e': "enter",
+    'i': "imes",
+    'a': "ai",
+    'o': "ober",
+    'u': "ufat",
+};
+const clavesDesencriptado = {
+    'enter': "e",
+    "imes": "i",
+    "ai": "a",  
+    "ober": "o",
+    "ufat": "u",
+}
 //Funcione Principales 
 
 function encriptacion() {
 
     // Capturar dato de entrada
     textoIngresado = document.getElementById('ingreso_texto_campo').value;
+    textoIngresado = textoIngresado.trim();
 
-    if (textoIngresado != null){
+    if (textoIngresado != ''){
 
+        textoEncriptado = transformarTexto(textoIngresado, clavesEncriptado);
         cardResultSetting();
-
         disappearImage();
-
         settingTitle('Encriptado');
-
-        printTextResult(textoIngresado);
-
+        
+        //console.log(textoEncriptado);
+        printTextResult(textoEncriptado);
         showCoppyButton();
-    } 
+    } else {
+        cardResultReset();
+    }
 
     // Limpiar area texto
     limpiarCampoTexto();
 }
 
 
+function desencriptacion() {
+    // Capturar dato de entrada
+    textoIngresado = document.getElementById('ingreso_texto_campo').value;
+    textoIngresado = textoIngresado.trim();
+
+    if (textoIngresado != '') {
+
+        textoDesencriptado = transformarTexto(textoIngresado, clavesDesencriptado);
+        cardResultSetting();
+        disappearImage();
+        settingTitle('Desencriptado');
+        
+        //console.log(textoEncriptado);
+        printTextResult(textoDesencriptado);
+        showCoppyButton();
+    } else {
+        cardResultReset();
+    }
+
+    // Limpiar area texto
+    limpiarCampoTexto();
+}
+
+function transformarTexto(texto, reemplazos) {
+    let textoTransformado = texto;
+    
+    for (let [original, reemplazo] of Object.entries(reemplazos)) {
+      // Escapar caracteres especiales en la expresión regular
+      const regex = new RegExp(original.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+      textoTransformado = textoTransformado.replace(regex, reemplazo);
+    }
+    
+    return textoTransformado;
+}
+
+function copiarTexto() {
+    // Obtén el texto del párrafo
+    const parrafo = document.getElementById('texto_mensaje');
+    const texto = parrafo.innerText;
+
+    // Usa la API del Portapapeles para copiar el texto
+    navigator.clipboard.writeText(texto).then(() => {
+        cardResultReset();
+    }).catch(err => {
+        console.error('Error al copiar el texto: ', err);
+    });
+
+    console.log("se copió");
+}
 
 // otras funciones
-function encriptarTexto() {}
 
 // atributos en la tarjetaResultado
 function cardResultSetting(){
@@ -62,6 +123,28 @@ function cardResultSetting(){
     //contenedorMensaje.style.gap = "32px";
 }
 
+
+function cardResultReset(){
+    tarjetaResultado.style.display="block";
+    tarjetaResultado.style.flex = "none";
+    tarjetaResultado.style.alignItems="center";
+    tarjetaResultado.style.justifyContent= "center";
+    tarjetaResultado.style.placeContent= "center";
+    
+    contenedorMensaje.style.display = "flex";
+    contenedorMensaje.style.flexDirection= "column";
+    contenedorMensaje.style.justifyContent = "center";
+    contenedorMensaje.style.alignItems = "center";
+    contenedorMensaje.style.textAlign="center";
+
+    showImage();
+    titleReset();
+    containerMensajeReset();
+    textReset();
+    hideCoppyButton();
+    
+
+}
 // Cambiar el titulo del resultado
 function settingTitle(estadoTexto){
     titulo.innerHTML = `Mensaje ${estadoTexto}`
@@ -70,9 +153,39 @@ function settingTitle(estadoTexto){
     titulo.style.height = "100%";
 }
 
+function titleReset(){
+    titulo.innerHTML = "Ningún mensaje fue encontrado";
+    titulo.style.fontSize= "36px";
+    titulo.style.fontWeight = "700";
+    titulo.style.marginTop = "1rem";
+    titulo.style.fontFamily = "Inter";
+    titulo.style.textAlign = "center";
+    titulo.style.width = "100%";
+    titulo.style.height= "100%";
+}
+
+function textReset(){
+    resultado.innerHTML = "Ingresa el texto que desees encriptar o desencriptar.";
+    resultado.style.fontSize= "1rem";
+    resultado.style.fontWeight = "400";
+    resultado.style.fontFamily= "Inter";
+    resultado.style.marginTop = "1rem";
+    resultado.style.textAlign = "center";
+    resultado.style.color = "--color3";
+    resultado.style.width = "80%";
+    resultado.style.height = "70%";
+}
+
+function containerMensajeReset(){
+    contenedorMensaje.style.display = "flex";
+    contenedorMensaje.style.flexDirection = "column";
+    contenedorMensaje.style.alignItems = "center";
+    contenedorMensaje.style.textAlign="center";
+    contenedorMensaje.style.width= "100%";
+}
 // texto a imprimir de resultado
 function printTextResult(texto){
-    resultado.innerHTML = textoIngresado;
+    resultado.innerHTML = texto;
     resultado.style.fontSize= "1.5rem";
     resultado.style.textAlign = "left";
     resultado.style.width = "100%";
@@ -87,12 +200,19 @@ function disappearImage(){
     imagen.style.display = "none";
 }
 
+function showImage(){
+    imagen.style.display="block";
+}
 // Boton Copiar
 function showCoppyButton(){
     btnCopiar.style.display = "block";
     btnCopiar.style.with= "70%";
     btnCopiar.style.color= "--color2";
     btnCopiar.style.with = "100%";
+}
+
+function hideCoppyButton(){
+    btnCopiar.style.display = "none";
 }
 
 function limpiarCampoTexto(){ document.querySelector("#ingreso_texto_campo").value = null;}
